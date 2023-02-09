4322c2f66f203a19e3121f51e42e93091f52f0a7
Vibilan Jayanth <vibilan@firstindo.com>
2023-02-09 21:19:09 +0800

    problem4

---
+++ src/problem4/retrieve-holders.ts
@@ -0,0 +1,24 @@
+import { ethers,  } from "ethers";
+
+const swthTokenContract = "0xc0ecb8499d8da2771abcbf4091db7f65158f1468";
+const provider = new ethers.providers.JsonRpcProvider("https://bsc-dataseed.binance.org/")
+
+const abi = [
+    "function balanceOf(address owner) view returns (uint256)"
+];
+
+const erc20 = new ethers.Contract(swthTokenContract, abi, provider);
+
+const addresses = [
+    "0xb5d4f343412dc8efb6ff599d790074d0f1e8d430",
+    "0x0020c5222a24e4a96b720c06b803fb8d34adc0af",
+    "0xd1d8b2aae2ebb2acf013b803bc3c24ca1303a392"
+];
+
+const print = async (address: string) : Promise<void> => {
+    const balance = await erc20.balanceOf(address);
+    const amount = ethers.utils.formatUnits(balance, 8);
+    console.log(`${address} ${amount}`);
+};
+
+addresses.map(print);
