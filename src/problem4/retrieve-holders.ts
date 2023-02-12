d9a4a3f16c7179c6fc2c091d0c62d57d49302103
Vibilan Jayanth <vibilan@firstindo.com>
2023-02-12 18:22:28 +0800

    added typing to problem4

--- src/problem4/retrieve-holders.ts
+++ src/problem4/retrieve-holders.ts
@@ -1,15 +1,15 @@
-import { ethers,  } from "ethers";
+import { ethers } from "ethers";
 
-const swthTokenContract = "0xc0ecb8499d8da2771abcbf4091db7f65158f1468";
-const provider = new ethers.providers.JsonRpcProvider("https://bsc-dataseed.binance.org/")
+const swthTokenContract: string = "0xc0ecb8499d8da2771abcbf4091db7f65158f1468";
+const provider: ethers.providers.JsonRpcProvider = new ethers.providers.JsonRpcProvider("https://bsc-dataseed.binance.org/")
 
-const abi = [
+const abi: string[] = [
     "function balanceOf(address owner) view returns (uint256)"
 ];
 
-const erc20 = new ethers.Contract(swthTokenContract, abi, provider);
+const erc20: ethers.Contract = new ethers.Contract(swthTokenContract, abi, provider);
 
-const addresses = [
+const addresses: string[] = [
     "0xb5d4f343412dc8efb6ff599d790074d0f1e8d430",
     "0x0020c5222a24e4a96b720c06b803fb8d34adc0af",
     "0xd1d8b2aae2ebb2acf013b803bc3c24ca1303a392"
